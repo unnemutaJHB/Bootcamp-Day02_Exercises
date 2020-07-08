782355893d646e24cf26e98c14db48f068510014
Unathi Nemutanzhela <unnemuta@c4r12s1.wethinkcode.co.za>
2020-07-08 16:23:25 +0200

    submitted ex00

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unnemuta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/08 11:34:54 by unnemuta          #+#    #+#             */
+/*   Updated: 2020/07/08 16:21:10 by unnemuta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c ,1);
+}
+void	ft_print_alphabet(void)
+{
+	char letter = 'a';
+	while(letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+	ft_putchar('\n');
+}
