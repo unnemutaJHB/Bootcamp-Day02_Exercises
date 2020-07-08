1f703c1094244ba6c1283dd09c6a8aa7aa53c679
Unathi Nemutanzhela <unnemuta@c4r12s1.wethinkcode.co.za>
2020-07-08 16:24:43 +0200

    submitted ex01

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unnemuta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/08 11:33:58 by unnemuta          #+#    #+#             */
+/*   Updated: 2020/07/08 16:24:03 by unnemuta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c ,1);
+}
+void	ft_print_reverse_alphabet(void)
+{
+	char letter = 'z';
+	while(letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter--;
+	}
+	ft_putchar('z');
+}
