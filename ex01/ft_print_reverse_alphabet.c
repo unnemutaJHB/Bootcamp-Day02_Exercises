c68f5f2f735eca897dd8165b7580328f86a32456
Unathi Nemutanzhela <unnemuta@c4r12s1.wethinkcode.co.za>
2020-07-10 12:15:25 +0200

    modified ex01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: unnemuta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 11:33:58 by unnemuta          #+#    #+#             */
-/*   Updated: 2020/07/08 16:24:03 by unnemuta         ###   ########.fr       */
+/*   Updated: 2020/07/10 12:13:59 by unnemuta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -23,5 +23,5 @@ void	ft_print_reverse_alphabet(void)
 		ft_putchar(letter);
 		letter--;
 	}
-	ft_putchar('z');
+	ft_putchar('\n');
 }
